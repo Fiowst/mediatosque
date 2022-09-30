0a6608cdbb528af2be78ba4e1a451acf4d3821b1
Clementine Bouillaud <clementine.bouillaud@laposte.net>
2022-09-30 10:36:37 +0200

    Creation du projet

---
+++ Mediatosque/src/main/java/com/plb/mediatosque/MediatosqueApplication.java
@@ -0,0 +1,13 @@
+package com.plb.mediatosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatosqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediatosqueApplication.class, args);
+	}
+
+}
