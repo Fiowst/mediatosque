407ea0943ec246288c4aa5109a340e9e93b74682
Fionna WESTBURY <fionna.westbury@gmail.com>
2022-10-24 17:42:10 +0200

    use case catalogue

--- Mediatosque/src/main/java/com/plb/mediatosque/service/ItemService.java
+++ Mediatosque/src/main/java/com/plb/mediatosque/service/ItemService.java
@@ -1,8 +1,35 @@
 package com.plb.mediatosque.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.plb.mediatosque.entity.Item;
+import com.plb.mediatosque.repository.ItemRepository;
 
 @Service
 public class ItemService {
 	
+	@Autowired
+	ItemRepository itemRepository;
+	
+	public List<Item> findItemByCategory(String category) {
+		List<Item> items;
+		switch (category) {
+			case "dvd" :
+				items = itemRepository.findAllDvd();
+				break;
+			case "cd" :
+				items = itemRepository.findAllCd();
+				break;
+			case "book" :
+				items = itemRepository.findAllBooks();
+				break;
+			default :
+				items = itemRepository.findAll();
+				break;
+		}
+		return items;
+	}
 }
--- Mediatosque/src/main/java/com/plb/mediatosque/web/rest/controller/ItemController.java
+++ Mediatosque/src/main/java/com/plb/mediatosque/web/rest/controller/ItemController.java
@@ -1,26 +1,48 @@
 package com.plb.mediatosque.web.rest.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plb.mediatosque.entity.Item;
 import com.plb.mediatosque.repository.ItemRepository;
+import com.plb.mediatosque.service.ItemService;
 
 @RestController
 @RequestMapping("/api/items")
 public class ItemController {
 	
-	private final ItemRepository itemRepository;
+	@Autowired
+	ItemRepository itemRepository;
 	
-	public ItemController(ItemRepository itemRepository) {
-		this.itemRepository=itemRepository;
+	@Autowired
+	ItemService itemService;
+	
+	// lister tous les documents
+	@GetMapping
+	public ResponseEntity<List<Item>> findAll(){
+		return ResponseEntity.ok(itemRepository.findAll());
 	}
 	
-	@GetMapping
-	public List<Item> findAll(){
-		return itemRepository.findAll();
+	// lister les documents disponibles
+	@GetMapping("/available")
+	public ResponseEntity<List<Item>> findAvailableItems() {
+		return ResponseEntity.ok(itemRepository.findAvailableItems());
+	}
+	
+	// lister les nouveaux documents
+	@GetMapping("/new")
+	public ResponseEntity<List<Item>> findNewItems() {
+		return ResponseEntity.ok(itemRepository.findNewItems(LocalDate.now().minusMonths(1)));
+	}
+	
+	@GetMapping("/category/{category}")
+	public ResponseEntity<List<Item>> findItemByCategory(String category) {
+		return ResponseEntity.ok(itemService.findItemByCategory(category.toLowerCase()));
 	}
 }
